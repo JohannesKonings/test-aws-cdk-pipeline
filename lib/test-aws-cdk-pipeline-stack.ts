a1d22774ce37cce304440363d6f13c80c84fe5c8
Johannes Konings <johannes.konings@outlook.com>
2022-02-02 14:33:33 +0000

    cdk setup

---
+++ lib/test-aws-cdk-pipeline-stack.ts
@@ -0,0 +1,16 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class TestAwsCdkPipelineStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'TestAwsCdkPipelineQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
