c6e5b778af0bf60cbec799edfb646a39083a168b
Johannes Konings <johannes.konings@outlook.com>
2022-02-02 18:12:07 +0000

    update pipeline setting

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source .env
+npm run cdk deploy
--- lib/test-aws-cdk-pipeline-stack.ts
+++ lib/test-aws-cdk-pipeline-stack.ts
@@ -3,14 +3,19 @@ import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 
 export class TestAwsCdkPipelineStack extends Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    console.log('codestar connection', process.env.CODESTAR_CONNECTION)
+
     const pipeline = new CodePipeline(this, 'Pipeline', {
-      pipelineName: 'MyPipeline',
+      pipelineName: 'jk-test-pipeline',
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('JohannesKonings/test-aws-cdk-pipeline', 'main'),
-        commands: ['npm ci', 'npm run build', 'npx cdk synth']
+        input: CodePipelineSource.connection('JohannesKonings/test-aws-cdk-pipeline', 'main', {
+          connectionArn: process.env.CODESTAR_CONNECTION as string
+        }),
+        // input: CodePipelineSource.gitHub('JohannesKonings/test-aws-cdk-pipeline', 'main'),
+        commands: ['npm install', 'npm run build', 'npx cdk synth']
       })
     });
   }
