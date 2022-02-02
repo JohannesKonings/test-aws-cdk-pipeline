dbba8be2902b8078e8c92035f7d1c736f12dfcfc
Johannes Konings <johannes.konings@outlook.com>
2022-02-02 14:40:55 +0000

    add cdk pipeline

--- bin/test-aws-cdk-pipeline.ts
+++ bin/test-aws-cdk-pipeline.ts
@@ -19,3 +19,5 @@ new TestAwsCdkPipelineStack(app, 'TestAwsCdkPipelineStack', {
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
+
+app.synth();
--- lib/test-aws-cdk-pipeline-stack.ts
+++ lib/test-aws-cdk-pipeline-stack.ts
@@ -1,16 +1,17 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 
 export class TestAwsCdkPipelineStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'TestAwsCdkPipelineQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const pipeline = new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'MyPipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('JohannesKonings/test-aws-cdk-pipeline', 'main'),
+        commands: ['npm ci', 'npm run build', 'npx cdk synth']
+      })
+    });
   }
 }
