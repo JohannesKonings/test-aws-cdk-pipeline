7b9f49b0713329397f98dca8de1720729c200eb5
Johannes Konings <johannes.konings@outlook.com>
2022-02-03 12:52:55 +0000

    wip

--- bin/test-aws-cdk-pipeline.ts
+++ bin/test-aws-cdk-pipeline.ts
@@ -2,9 +2,11 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { TestAwsCdkPipelineStack } from '../lib/test-aws-cdk-pipeline-stack';
+import { Tags } from 'aws-cdk-lib';
 
 const app = new cdk.App();
-new TestAwsCdkPipelineStack(app, 'TestAwsCdkPipelineStack', {
+const name = 'TestAwsCdkPipelineStack'
+new TestAwsCdkPipelineStack(app, name, {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -19,5 +21,7 @@ new TestAwsCdkPipelineStack(app, 'TestAwsCdkPipelineStack', {
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
+
+Tags.of(app).add('Project', name);
 
 app.synth();
--- lib/test-aws-cdk-pipeline-stack.ts
+++ lib/test-aws-cdk-pipeline-stack.ts
@@ -1,22 +1,27 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { MyPipelineAppStage } from './pipeline-app';
 
 export class TestAwsCdkPipelineStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    console.log('codestar connection', process.env.CODESTAR_CONNECTION)
-
     const pipeline = new CodePipeline(this, 'Pipeline', {
+      crossAccountKeys: true,
       pipelineName: 'jk-test-pipeline',
+      selfMutation: false,
       synth: new ShellStep('Synth', {
         input: CodePipelineSource.connection('JohannesKonings/test-aws-cdk-pipeline', 'main', {
           connectionArn: process.env.CODESTAR_CONNECTION as string
         }),
-        // input: CodePipelineSource.gitHub('JohannesKonings/test-aws-cdk-pipeline', 'main'),
         commands: ['npm install', 'npm run build', 'npx cdk synth']
       })
     });
+
+    pipeline.addStage(new MyPipelineAppStage(this, "test", {
+
+    }));
+
   }
 }
